01ff01b33f2b24233c602d1f52d2b944cc08fbd0
livx5199 <livx5199@stud.kea.dk>
2024-10-24 19:15:55 +0200

    header+footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+	theme: 
+    {
+        extend: {
+            fontFamily: {
+                codystar: ["Codystar", "sans-serif"],
+              },
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'pc-coral': '#FF1054',
+              },
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
